66a28f0c22dbeb0646dfaaee6df1677195716c4a
Nauhith21 <22B81A0593@cvr.ac.in>
2025-01-02 10:50:08 +0530

    First Commit

---
+++ ArrayDes.js
@@ -0,0 +1,9 @@
+// Create an Array
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// Destructuring Assignment
+let [fruit1, fruit2] = fruits;
+console.log(fruit1)
+
+var [fruit,...fruit_list] = fruits
+console.log(fruit_list)
